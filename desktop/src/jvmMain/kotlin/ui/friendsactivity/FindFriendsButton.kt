a6e7a6979cbbb8ab61ef608816970b80927d5ac8
Kuba <jakub.zmidzinski@gmail.com>
2021-01-24 12:46:38 +0100

    Rounds Find Friends button

--- desktop/src/jvmMain/kotlin/ui/friendsactivity/FindFriendsButton.kt
+++ desktop/src/jvmMain/kotlin/ui/friendsactivity/FindFriendsButton.kt
@@ -1,16 +1,38 @@
 package ui.friendsactivity
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@ExperimentalMaterialApi
 @Composable
 fun FindFriendsButton() {
     Button(
-        onClick = {}
+        onClick = {},
+        modifier = Modifier
+            .background(Color.Transparent),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Transparent,
+            contentColor = Color.White
+        ),
+        border = BorderStroke(1.dp, Color.White),
+        shape = RoundedCornerShape(16.dp)
     ) {
         Text(
-            text = "Find Friends".toUpperCase()
+            text = "Find Friends".toUpperCase(),
+            fontSize = 10.sp,
+            modifier = Modifier
+                .padding(horizontal = 24.dp)
         )
     }
 }
